f46434c2f5a9a7e39022ecb1b3f215c7dfd7ad33
luqiankun <lqk1949@foxmail.com>
2025-08-18 11:57:31 +0800

    imu fix

---
+++ src/move_ctrl/include/move.hpp
@@ -0,0 +1,8 @@
+#ifndef MOVE_H
+#define MOVE_H
+#include <can_msgs/msg/frame.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
+class MoveCtrl {};
+class Odomtry {};
+#endif
